550dc13112534dd7f24258153077d011e5477429
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-03-04 23:25:05 +0530

    separated home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:cdli_tablet/home_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: "CDLI Tablet",
+      home: HomeScreen(),
+    );
   }
 }
